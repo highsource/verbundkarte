b18f706a80610350a0d520fc718521bfd2123a26
Max Buchholz <Max.Buchholz@gmx.de>
2024-08-12 16:54:16 +0200

    Fix links in Download scripts

--- Verwaltungsgrenzen/download.bat
+++ Verwaltungsgrenzen/download.bat
@@ -1,3 +1,3 @@
-wget http://sg.geodatenzentrum.de/web_download/vg/vg1000_0101/gk3/shape/vg1000_0101.gk3.shape.ebenen.zip
-unzip -jo vg1000_0101.gk3.shape.ebenen.zip vg1000_0101.gk3.shape.ebenen/vg1000_ebenen/*
-del vg1000_0101.gk3.shape.ebenen.zip
+wget https://daten.gdz.bkg.bund.de/produkte/vg/vg1000_ebenen_0101/aktuell/vg1000_01-01.gk3.shape.ebenen.zip
+unzip -jo vg1000_01-01.gk3.shape.ebenen.zip vg1000_01-01.gk3.shape.ebenen/vg1000_ebenen/*
+del vg1000_01-01.gk3.shape.ebenen.zip
